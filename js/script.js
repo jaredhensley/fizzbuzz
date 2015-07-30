0c1ef41fd56e9e2c340b84af383443d6258a519c
Jared Hensley <jahensle85@me.com>
2015-07-29 20:09:47 -0400

    fizzbuzz template unstyled

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 $(document).ready(function() {
 
-for (var i = 1; i <= 100; i++) {
-	if (i % 5 === 0 && i % 3 === 0) { //check to see if i is divisible by 5 AND 3
-		$("body").append("FizzBuzz <br>");
-		console.log("FizzBuzz");
-	} else if (i % 5 === 0) { //check to see if i is divisible by 5
-		$("body").append("buzz <br>");
-		console.log("buzz");
-	} else if (i % 3 === 0) { //check to see if i is divisible by 3
-		$("body").append("fizz <br>");
-		console.log("fizz");
-	} else {
-		$("body").append(i + "<br>");
-		console.log(i); //if i doesn't fall under the 3 cases below, print 
-	}
+	$("button").on("click", function() {
 
-}
+		var highNumber = document.getElementById('highNumber').value;  //chose to go with vanilla js here just instead of jquery, just something different.
 
+
+		var fizzbuzz = function (arg) {
+
+		for (var i = 1; i <= arg; i++) {
+			if (i % 5 === 0 && i % 3 === 0) { //check to see if i is divisible by 5 AND 3
+				$(".output").append("FizzBuzz <br>");
+				console.log("FizzBuzz");
+			} else if (i % 5 === 0) { //check to see if i is divisible by 5
+				$(".output").append("buzz <br>");
+				console.log("buzz");
+			} else if (i % 3 === 0) { //check to see if i is divisible by 3
+				$(".output").append("fizz <br>");
+				console.log("fizz");
+			} else {
+				$(".output").append(i + "<br>");
+				console.log(i); //if i doesn't fall under the 3 cases below, print 
+			}
+
+		}
+
+
+		}
+
+	fizzbuzz(highNumber);
+
+	});
+	
 });
