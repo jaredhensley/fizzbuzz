3b62eb2484881e7ef659c0b7b2fdfa9ea5afa785
Jared Hensley <jahensle85@me.com>
2015-07-29 20:20:05 -0400

    clear output div on second submission

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-
+	
 	$("button").on("click", function() {
 
+		$(".output").empty();
 		var highNumber = document.getElementById('highNumber').value;  //chose to go with vanilla js here just instead of jquery, just something different.
 
-
 		var fizzbuzz = function (arg) {
-
 		for (var i = 1; i <= arg; i++) {
 			if (i % 5 === 0 && i % 3 === 0) { //check to see if i is divisible by 5 AND 3
 				$(".output").append("FizzBuzz <br>");
@@ -21,10 +20,7 @@ $(document).ready(function() {
 				$(".output").append(i + "<br>");
 				console.log(i); //if i doesn't fall under the 3 cases below, print 
 			}
-
 		}
-
-
 		}
 
 	fizzbuzz(highNumber);
@@ -35,7 +31,5 @@ $(document).ready(function() {
 		if(e.which == 13) {
 			$('button').click();
 		}
-
 	});
-
 });
